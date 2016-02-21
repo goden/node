4f536408b63d89a04cfd367bb7bbf0d2dae0132d
goden690426 <goden.incredible@gmail.com>
2016-02-21 20:15:57 +0000

    create http server that has html output.

--- exercise/4-2/app.js
+++ exercise/4-2/app.js
@@ -1,9 +1,10 @@
 var http = require("http");
 http.createServer(function(request, response) {
 
-
-    response.write("Node.JS!");
-
-
+    response.write("<html>");
+    response.write("<body>");
+    response.write("<h1>Node.JS!</h1>");
+    response.write("</body>");
+    response.write("</html>");
     response.end();
 }).listen(process.env.PORT || 3000);
