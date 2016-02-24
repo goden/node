9f3f2a3a11ecd1b0547079a4efca2efbcb8f6da3
goden690426 <goden.incredible@gmail.com>
2016-02-24 09:39:18 +0000

    Read file by given path using mine package.

--- exercise/4-8/app.js
+++ exercise/4-8/app.js
@@ -1,29 +1,37 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var mine = require("mine");
 
 http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (pathname.substr(pathname.length - 1) === "/") {
+        pathname += "index.html";
+    }
+    
+    pathname = "." + pathname;
+    
+    try{
+        if (fs.statSync(pathname).isFile()) {
+            response.writeHead(200, {
+                "Content-Type": mine.lookup(pathname)
+            });
+            response.write(fs.readFileSync(pathname));
+            response.end();
+        } else {
+            handleAsNotFound();
+        }
+        
+    } catch (e) {
+        handleAsNotFound();
+    }
+    
+    function handleAsNotFound() {
+        response.writeHead(404);
+        response.write("404 not found.");
+        response.end();
+    }
 
 
 }).listen(process.env.PORT || 3000);
