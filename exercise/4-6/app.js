a18494ae81c3868ac20917c14180c86aed80386d
goden690426 <goden.incredible@gmail.com>
2016-02-24 07:22:02 +0000

    add condition to handle if file not found.

--- exercise/4-6/app.js
+++ exercise/4-6/app.js
@@ -1,36 +1,46 @@
 var http = require("http");
 http.createServer(function(request, response) {
+    var statusCode = 200;
     var html="", contentType="";
     if (request.url === "/") {
         contentType = "text/html";
         html = `<html>
-<head>
-<link type="text/css" rel="stylesheet" href="index.css">
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="index.js"></script>
-</head>
-<body>
-    <div>請按我!</div>
-</body>
-</html>`;
+            <head>
+            <link type="text/css" rel="stylesheet" href="index.css">
+            <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+            <script src="index.js"></script>
+            </head>
+            <body>
+                <div>請按我!</div>
+            </body>
+            </html>`;
     } else if (request.url === "/index.css") {
         contentType = "text/css";
         html = `div {
-    color:green;
-    font-size:50px;
-    cursor:pointer;
-}`;
+            color:green;
+            font-size:50px;
+            cursor:pointer;
+        }`;
     } else if (request.url === "/index.js") {
         contentType = "application/x-javascript";
         html = `$(function() {
-    $("div").click(function() {
-        alert("我被按下了!");
-    });
-});`;
+            $("div").click(function() {
+                alert("我被按下了!");
+            });
+        });`;
+    } else {
+        statusCode = 404;
     }
-    response.writeHead(200, {
-        "Content-Type": contentType
-    });
-    response.write(html);
+    
+    if (statusCode === 404) {
+        response.writeHead(404);
+        response.write("404 not found.");
+    } else {
+        response.writeHead(statusCode, {
+            "Content-Type": contentType
+        });
+        response.write(html);
+    }
+
     response.end();
 }).listen(process.env.PORT || 3000);
