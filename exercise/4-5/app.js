e9b73f9366cef017c5fa5da217f5344101cb47b0
goden690426 <goden.incredible@gmail.com>
2016-02-24 06:46:56 +0000

    Response the according content by given url.

--- exercise/4-5/app.js
+++ exercise/4-5/app.js
@@ -1,30 +1,39 @@
 var http = require("http");
 http.createServer(function(request, response) {
-    response.writeHead(200, {
-        "Content-Type": "text/html"
-    });
-    var html = `<html>
-<head>
-<style>
-    div {
-        color:green;
-        font-size:50px;
-        cursor:pointer;
+    
+    var html  = "", contentType = "";
+    if (request.url === "/") {
+        contentType = "text/html";
+        html = `<html>
+                <head>
+                <link type="text/css" rel="stylesheet" href="index.css">
+                <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+                <script src="index.js"></script>
+                </head>
+                <body>
+                    <div>請按我!</div>
+                </body>
+                </html>`;
+    } else if (request.url === "/index.css") {
+        contentType = "text/css";
+        html = `div {
+            color:green;
+            font-size:50px;
+            cursor:pointer;
+        }`;
+    } else if (request.url === "/index.js") {
+        contentType = "application/x-javascript";
+        html = `$(function() {
+            $("div").click(function() {
+                alert("我被按下了!");
+            });
+        });`;
     }
-</style>
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script>
-    $(function() {
-        $("div").click(function() {
-            alert("我被按下了!")
-        });
+    
+    response.writeHead(200, {
+        "Content-Type": contentType
     });
-</script>
-</head>
-<body>
-    <div>請按我!</div>
-</body>
-</html>`;
+
     response.write(html);
     response.end();
 }).listen(process.env.PORT || 3000);
