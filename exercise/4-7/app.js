fb0874f7a91c2508ac3385d2eeba95c57e3e7e24
goden690426 <goden.incredible@gmail.com>
2016-02-24 08:41:00 +0000

    read file by given path

--- exercise/4-7/app.js
+++ exercise/4-7/app.js
@@ -1,8 +1,11 @@
 var http = require("http");
+var fs = require("fs");
+var url =  require("url");
 
 
 http.createServer(function(request, response) {
     var contentType;
+    var pathname = url.parse(request.url).pathname;
     
     var routes = {
         "/": function() {
@@ -23,15 +26,20 @@ http.createServer(function(request, response) {
         }
     };
 
-
-
-
-
-
-
-
-
-
-
+    if (typeof routes[pathname] === "function") {
+        var data = routes[pathname]();
+        response.writeHead("200", {
+            "Content-Type": contentType
+        });
+        response.write(data);
+        response.end();
+        
+    } else {
+        
+        response.writeHead(404);
+        response.write("404 not found.");
+        response.end();
+        
+    }
 
 }).listen(process.env.PORT || 3000);
