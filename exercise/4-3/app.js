6eff5d8825c114c3609ce9dbbc85c53c889288e1
goden690426 <goden.incredible@gmail.com>
2016-02-25 08:05:36 +0000

    修正response時輸出的html內容

--- exercise/4-3/app.js
+++ exercise/4-3/app.js
@@ -4,12 +4,31 @@ http.createServer(function(request, response) {
     response.writeHead(200, {
         "Content-Type" : "text/html"
     });
+    
+    var html = `<html>
+<head>
+<style>
+    div {
+        color:green;
+        font-size:50px;
+        cursor:pointer;
+    }
+</style>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script>
+    $(function() {
+        $("div").click(function() {
+            alert("我被按下了!")
+        });
+    });
+</script>
+</head>
+<body>
+    <div>請按我!</div>
+</body>
+</html>`;
 
-    response.write("<html>");
-    response.write("<body>");
-    response.write("<h1>Node.JS!</h1>");
-    response.write("</body>");
-    response.write("</html>");
+    response.write(html);
     response.end();
     
 }).listen(process.env.PORT || 3000);
