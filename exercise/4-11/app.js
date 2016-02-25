0c1e9aa5cc98f2596cad0a88ca60ccfd9a84894a
goden690426 <goden.incredible@gmail.com>
2016-02-25 08:21:33 +0000

    因為中文網址會被編碼，使用decodeURI函式還原編碼過的中文字

--- exercise/4-11/app.js
+++ exercise/4-11/app.js
@@ -17,6 +17,7 @@ http.createServer(function(request, response) {
         }
     }
     pathname = (process.argv[2] || ".") + pathname; //若有傳入參數則使用參數的路徑
+    pathname = decodeURI(pathname);                 //因為中文網址會被編碼，使用此原生函式可以還原編碼過的中文字
 
     try {
         if (fs.statSync(pathname).isFile()) {
