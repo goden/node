14d049ffd5e0a3ff26e6872d6d852f7627aa7c2f
goden690426 <goden.incredible@gmail.com>
2016-02-25 08:07:23 +0000

    依路徑讀取檔案使其自動判別路徑，判別非檔案時即導301

--- exercise/4-10/app.js
+++ exercise/4-10/app.js
@@ -6,15 +6,18 @@ http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
     if (pathname.substr(pathname.length - 1) === "/") {
         pathname += "index.html"; //若無帶入檔名預設為index.html
+    } else {
+
+        var paths = pathname.split("/").pop();
+        if (paths.indexOf(".") === -1) {
+            response.writeHead(301, {
+                "Location": pathname + "/" + (url.parse(request.url).search || "")
+            });
+            response.end();
+            return;
+        }
+        
     }
-
-
-
-
-
-
-
-
 
     pathname = (process.argv[2] || ".") + pathname; //若有傳入參數則使用參數的路徑
     try {
