f744fceb9f5d9c06b53dea11d64b5dabe7573e72
goden690426 <goden.incredible@gmail.com>
2016-02-28 07:24:45 +0000

    use post method to implement, use querystring module to parse a string to object.

--- exercise/6-5/api.js
+++ exercise/6-5/api.js
@@ -1,13 +1,20 @@
 var util = require("util");
 var url = require("url");
-
+var querystring =  require("querystring");
 module.exports = function(request, response) {
     var pathname = url.parse(request.url).pathname;
     if (pathname === "/api/login/" && request.method === "POST") {
 
+        var postdata = "";
+        request.on("data", (data) => {
+            postdata += data;
+        });
 
-
-
+        request.on("end", () => {
+            console.log(postdata + " => " + util.inspect(querystring.parse(postdata)));
+            response.write("OK");
+            response.end();
+        });
 
 
 
