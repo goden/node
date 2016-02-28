d43579a1a8d44d25c1fe23a2da48e4909938a20d
goden690426 <goden.incredible@gmail.com>
2016-02-28 04:59:39 +0000

    create a http server which returns a json data.

--- exercise/6-1/app.js
+++ exercise/6-1/app.js
@@ -1,7 +1,12 @@
+// 模組化回傳JSON資料
+//
 var http = require("http");
-
+var api = require("./api");
+var static = require("./static");       // 還是要儘可能少用關鍵字作為變數名稱
 
 http.createServer(function(request, response) {
 
+    api(request, response);
+    static(request, response);
 
 }).listen(process.env.PORT || 3000);
