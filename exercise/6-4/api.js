bdfd90d1eed78f4a791caba85ff0259617ae939e
goden690426 <goden.incredible@gmail.com>
2016-02-28 06:57:43 +0000

    guess number using get mothod to implement.

--- exercise/6-4/api.js
+++ exercise/6-4/api.js
@@ -1,12 +1,33 @@
 var url = require("url");
 
+var number;
 module.exports = function(request, response) {
     var pathname = url.parse(request.url).pathname;
     var query = url.parse(request.url, true).query;
     if (pathname === "/api/") {
 
+        if (!number) {
+            number = ~~(Math.random() * 10000);
+        }
+        
+        var num = query.num || 0;
+        var output = {};
+        if (num > number) {
+            output.message = "比" + num + "小";
+        } else if (num < number) {
+            output.message = "比" + num + "大";
+        } else {
+            output.message = "猜中了! 是" + num + "!"
+            output.success = true;
+            number = ~~(Math.random() * 10000);
+        }
+        
+        response.writeHead(200, {
+            "Content-Type": "application/json"
+        });
 
-
+        response.write(JSON.stringify(output));
+        response.end();
 
 
 
